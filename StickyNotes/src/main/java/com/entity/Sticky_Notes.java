1f11875b03f4977bc259af0d0662a74ec6a0a7b0
Madhurmoms <madhurjatiya13@gmail.com>
2023-02-23 00:18:52 +0530

    1st commit

---
+++ StickyNotes/src/main/java/com/entity/Sticky_Notes.java
@@ -0,0 +1,70 @@
+package com.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Notes")
+public class Sticky_Notes {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	private String content;
+	private Date addedDate;
+
+	public Sticky_Notes(int id, String title, String content, Date addedDate) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.addedDate = addedDate;
+	}
+
+	public Sticky_Notes() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Sticky_Notes [id=" + id + ", title=" + title + ", content=" + content + ", addedDate=" + addedDate
+				+ "]";
+	}
+}
