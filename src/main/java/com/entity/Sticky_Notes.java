b99aab0f638c7d7b6b9387af655831a214191ad8
Madhurmoms <madhurjatiya13@gmail.com>
2023-02-23 00:31:33 +0530

    2nd commit

--- src/main/java/com/entity/Sticky_Notes.java
+++ src/main/java/com/entity/Sticky_Notes.java
@@ -2,6 +2,7 @@ package com.entity;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,7 @@ import jakarta.persistence.Table;
 public class Sticky_Notes {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Notes_id")
 	private int id;
 	private String title;
 	private String content;
