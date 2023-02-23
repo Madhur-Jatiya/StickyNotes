7dc57911585b6b545396986637d999ff2b3320ad
Madhurmoms <madhurjatiya13@gmail.com>
2023-02-23 13:22:16 +0530

    add jsp and css new files

--- src/main/java/com/helper/FactoryProvider.java
+++ src/main/java/com/helper/FactoryProvider.java
@@ -1,5 +1,21 @@
 package com.helper;
 
-public class FactoryProvider {
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+public class FactoryProvider {
+	public static SessionFactory sessionFactory;
+	
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			sessionFactory = new Configuration().configure("config.xml").buildSessionFactory();
+		}
+		return sessionFactory;
+	}
+	
+	public void closeFactory() {
+		if (sessionFactory.isOpen()) {
+			sessionFactory.close();
+		}
+	}
 }
